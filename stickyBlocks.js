ad702cc88de314ba63596c5038cc840dcfcfa180
loze <ilgstudio.com>
2017-03-20 13:37:31 -0400

    added js file

---
+++ stickyBlocks.js
@@ -0,0 +1,285 @@
+// stickyBlocks
+// Sticks blocks to its parent when scrolling using the available width between two elements.
+// Sort of like the right column on facebook
+// requires waypoints.js
+// 3/18/17
+// by jpelosi
+
+
+(function($) {
+
+  $.stickMultiBlocks = function(element, options) {
+
+    // plugin's default options
+    // this is private property and is  accessible only from inside the plugin
+    var defaults = {
+      blockElements: '.block', // the elements inside this container to make sticky
+      stuckClass: 'stickyBlock', // class to add to stuck elements.
+      offset: 0, // offset pixels from the top of the page.
+      topTrigger: 'body', // the top of this element is the trigger
+      bottomTrigger: 'body' // the bottom of this element is the trigger
+
+      // if your plugin is event-driven, you may provide callback capabilities for its events.
+      // execute these functions before or after events of your plugin, so that users may customize
+      // those particular events without changing the plugin's code
+      // onFoo: function() {}
+    }
+
+    // to avoid confusions, use "plugin" to reference the current instance of the object
+    var plugin = this;
+
+    // this will hold the merged default, and user-provided options
+    // plugin's properties will be available through this object like:
+    // plugin.settings.propertyName from inside the plugin or
+    // element.data('stickMultiBlocks').settings.propertyName from outside the plugin, where "element" is the
+    // element the plugin is attached to;
+    plugin.settings = {}
+
+    var $element = $(element), // reference to the jQuery version of DOM element the plugin is attached to
+      element = element; // reference to the actual DOM element
+
+    // ====================================================================
+    // the "constructor" method that gets called when the object is created
+    plugin.init = function() {
+
+      // the plugin's final properties are the merged default and user-provided options (if any)
+      plugin.settings = $.extend({}, defaults, options);
+
+      var $blocks = $element.find(plugin.settings.blockElements);
+      if (!$blocks.length) {
+        return false;
+      }
+      // code goes here
+      plugin.totalBlockHeight = 0;
+      //var $container = $element; //$(this);
+      plugin.sticking = false;
+      plugin.bottomEl = $(plugin.settings.bottomTrigger);
+      plugin.topEl = $(plugin.settings.topTrigger);
+      plugin.stuckBlock = false;
+      //var enabled = true;
+      /*if (!$element.length) {
+        return false;
+      }*/
+
+      plugin.wrappers = wrapElements($blocks);
+      plugin.calcBlocks();
+      //console.log('totalBlockHeight: ' + plugin.totalBlockHeight);
+
+      //console.log($blocks);
+      //console.log($wrappers);
+
+      plugin.waypoint = $(plugin.settings.topTrigger).waypoint({
+        handler: function(direction) {
+          if (direction == 'down') {
+            // start sticking
+            //console.log('Start Sticking');
+            plugin.sticking = true;
+            //$el.addClass(settings.stuckClass);
+          } else {
+            // stop sticking
+            //console.log('Stop Sticking');
+            sticking = false;
+            unstick(plugin.wrappers);
+            //$el.removeClass(settings.stuckClass);
+          }
+        },
+        offset: plugin.settings.offset //$('#navigation').height()
+      });
+
+      $(window).scroll(plugin.react);
+
+    }
+
+    // ==================================================================
+    // public methods
+    // these methods can be called like:
+    // plugin.methodName(arg1, arg2, ... argn) from inside the plugin or
+    // element.data('stickMultiBlocks').publicMethod(arg1, arg2, ... argn) from outside the plugin, where "element"
+    // is the element the plugin is attached to;
+
+    // a public method. for demonstration purposes only - remove it!
+/*    plugin.foo_public_method = function() {
+
+      // code goes here
+
+    }*/
+
+
+    plugin.calcBlocks = function() {
+      if (plugin.wrappers.length) {
+        unstick(plugin.wrappers);
+        plugin.wrappers.each(function(index, el) {
+          $el = $(el);
+          $el.data('origHeight', $el.outerHeight());
+          $el.data('origWidth', $el.outerWidth());
+        });
+        plugin.totalBlockHeight = $element.outerHeight(true);
+      }
+    }
+
+
+    plugin.react = function() {
+
+      if (plugin.sticking) {
+        //console.log('reacting');
+        //  if($('#content-inner').outerHeight(true) > fstop.totalSidebarHeightRight){
+        //
+        var stageHeight = plugin.bottomEl.outerHeight(true); //bottomEl.offset().top + bottomEl.outerHeight(true) - topEl.offset().top;
+        if (stageHeight > plugin.totalBlockHeight) {
+          var $vis = getVisible(availableHeight(), plugin.wrappers);
+          var scrollTop = $(window).scrollTop();
+          if ($vis) {
+            if (scrollTop > plugin.totalBlockHeight + $element.offset().top - $vis.outerHeight(true) - plugin.settings.offset - 5) {
+              //console.log('sticking', $vis);
+              if ($vis != plugin.stuckBlock) {
+                unstick($element.find('.' + plugin.settings.stuckClass).not($vis));
+                if (!$vis.hasClass(plugin.settings.stuckClass)) {
+                  stick($vis);
+
+                }
+              }
+
+            } else {
+              unstick($element.find('.' + plugin.settings.stuckClass));
+            }
+          } else {
+            unstick($element.find('.' + plugin.settings.stuckClass));
+          }
+        }
+      }
+    }
+
+
+    plugin.enable = function() {
+      $.each(plugin.waypoint, function(i, wp) {
+        wp.enable();
+        plugin.sticking = true; //wp.refresh();
+      });
+    }
+
+
+    plugin.disable = function() {
+      $.each(plugin.waypoint, function(i, wp) {
+        wp.disable();
+      });
+      plugin.sticking = false;
+      unstick(plugin.wrappers);
+    }
+
+    // ==============================================================
+    // private methods
+    // these methods can be called only from inside the plugin like:
+    // methodName(arg1, arg2, ... argn)
+
+    // a private method. for demonstration purposes only - remove it!
+    /*var foo_private_method = function() {
+
+      // code goes here
+
+    }*/
+
+    var unstick = function(block) {
+      //if (block.length) {
+      block.removeClass(plugin.settings.stuckClass);
+      block.removeAttr('style');
+      //console.log('unsticking', block);
+      //}
+    }
+
+    var stick = function(block) {
+      block.addClass(plugin.settings.stuckClass);
+      block.css({
+        //'position': 'fixed',
+        'top': plugin.settings.offset,
+        'width': block.data('origWidth') + 'px'
+      });
+      plugin.stuckBlock = block;
+    }
+
+    var wrapElements = function(blocks) {
+      var wrapped = $();
+      blocks.wrapAll($('<div class="scrollgroup"></div>'));
+      blocks.each(function(index, el) {
+        wrapped = wrapped.add(el);
+        blocks.not(wrapped).wrapAll($('<div class="scrollgroup"></div>'));
+      });
+      // return all the wrapped divs
+      //console.log(blocks);
+      return $element.find('.scrollgroup');
+    }
+
+
+    var getVisible = function(av, blocks) {
+      var visEl = false;
+      // console.log(settings);
+      blocks.each(function(index, el) {
+        var $el = $(el);
+        //console.log($element.data('origHeight') + ' < ' + av);
+        //// data('origHeight')
+        if ($el.data('origHeight') < av) {
+          //console.log(index);
+          visEl = $el;
+          return false;
+        }
+      });
+      return visEl;
+    }
+
+    var availableHeight = function() {
+      var scrollTop = $(window).scrollTop();
+      var availHeight = Waypoint.viewportHeight() - plugin.settings.offset;
+      //console.log(stickTop);
+      //var bottom = bottomEl.offset().top + bottomEl.outerHeight(true) - topEl.position().top;
+      //console.log(bottom);
+      if (plugin.bottomEl.length) {
+        //var footerPos = fstop.footer.offset().top - scrollTop - navHeight;
+        var bottomPos = plugin.bottomEl.offset().top + plugin.bottomEl.outerHeight(true) - scrollTop - plugin.settings.offset; //20; //scrollTop -
+
+        if (bottomPos < availHeight) {
+          availHeight = bottomPos;
+        }
+      }
+
+      availHeight = Math.round(availHeight);
+      //console.log(availHeight);
+      return availHeight;
+    }
+
+
+    // fire up the plugin!
+    // call the "constructor" method
+    plugin.init();
+
+  }
+
+
+  // add the plugin to the jQuery.fn object
+  $.fn.stickMultiBlocks = function(options) {
+
+    // iterate through the DOM elements we are attaching the plugin to
+    return this.each(function() {
+
+      // if plugin has not already been attached to the element
+      if (undefined == $(this).data('stickMultiBlocks')) {
+
+        // create a new instance of the plugin
+        // pass the DOM element and the user-provided options as arguments
+        var plugin = new $.stickMultiBlocks(this, options);
+
+        // in the jQuery version of the element
+        // store a reference to the plugin object
+        // you can later access the plugin and its methods and properties like
+        // element.data('stickMultiBlocks').publicMethod(arg1, arg2, ... argn) or
+        // element.data('stickMultiBlocks').settings.propertyName
+        $(this).data('stickMultiBlocks', plugin);
+
+        //@@todo. how can i return the pugin functions directly to the object.
+        //$(this).extend(plugin);
+
+      }
+
+    });
+
+  }
+
+})(jQuery);
